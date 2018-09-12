6cefd1f9e2028e7760afc898d04c5e3a0d703143
Ming-Yuan Yu <myyu@umich.edu>
2018-09-12 04:53:40 -0400

    update config.fish

--- config.fish
+++ config.fish
@@ -3,7 +3,9 @@
 set fish_greeting ""
 
 ##  Some handy aliases
-alias ls='ls --color=auto'
-alias ll='ls -halF'
-alias gm='watch -t -n 0.2 nvidia-smi'
-alias htop='htop -d 2'
+alias ls='ls --color=always'
+#alias ll='ls -halF'
+alias gm="watch -t -n 0.5 'hostname && nvidia-smi'"
+alias ct="watch -t -n 0.5 'hostname && date && sensors | grep id | grep -iv fan'"
+alias htop="htop -d 5"
+alias pylab="ipython3 --pylab"
